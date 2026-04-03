be210874a7f562856ca9a715a7caebce578c2391
Garrett Polinsky <garrett@geelinsky.com>
2026-04-03 12:30:21 -0600

    Update ComponentShowcase to reflect new user profile for @GeeLinsky, including updated avatar image and bio. This enhances the showcase with accurate representation and personal branding.

--- src/components/component-showcase/ComponentShowcase.tsx
+++ src/components/component-showcase/ComponentShowcase.tsx
@@ -1671,20 +1671,20 @@ const ComponentShowcase = () => {
               <CardContent className="flex justify-center">
                 <HoverCard>
                   <HoverCardTrigger>
-                    <Button variant="link">@nextjs</Button>
+                    <Button variant="link">@GeeLinsky</Button>
                   </HoverCardTrigger>
                   <HoverCardContent className="w-80">
                     <div className="flex justify-between space-x-4">
                       <Avatar>
-                        <AvatarImage src="/placeholder.svg?height=40&width=40" />
-                        <AvatarFallback>VC</AvatarFallback>
+                        <AvatarImage src="https://statix.geelinsky.com/images/garrett-headshot.jpeg" />
+                        <AvatarFallback>GP</AvatarFallback>
                       </Avatar>
                       <div className="space-y-1">
-                        <h4 className="text-sm font-semibold">@nextjs</h4>
-                        <p className="text-sm">The React Framework – created and maintained by @vercel.</p>
+                        <h4 className="text-sm font-semibold">@GeeLinsky</h4>
+                        <p className="text-sm">Staff Software Engineer building web apps in Salt Lake City.</p>
                         <div className="flex items-center pt-2">
                           <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
-                          <span className="text-xs text-muted-foreground">Joined December 2021</span>
+                          <span className="text-xs text-muted-foreground">Salt Lake City, Utah</span>
                         </div>
                       </div>
                     </div>
